f462f1a2fffc910b16148b71c1638d531fa40418
wubaiqing <wubaiqing@vip.qq.com>
2014-04-08 22:45:12 +0800

    添加美品网关键字，描述，标题，cnzz信息

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,76 +1,259 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<meta charset="utf-8">
-		<meta name="keywords" content="51促销网,51促销网独家优惠,51促销网官网,51促销网官方网站,zhe800,51促销网">
-		<meta name="description" content="51促销网，每日搜罗全网最值得买的打折商品，汇集51促销网、九块邮、会员购等精品打折商品">
-		<title><?php echo empty($this->pageTitle) ? '51促销网_折800网、九块邮、会员购等9.9包邮精选' : $this->pageTitle;?></title>
-		<link rel="shortcut icon" href="http://www.40zhe.com/static/images/favicon.ico?v=wubaiqing1.5" type="image/x-icon">
-		<link rel="stylesheet" type="text/css" href="http://www.40zhe.com/static/sale.css?v=wubaiqing1.7.1">
-		<link rel="stylesheet" type="text/css" href="http://www.40zhe.com/static/sale_cuxiao.css?v=master">
-		<script src="http://libs.baidu.com/jquery/1.9.0/jquery.min.js"></script>
-		<script type="text/javascript" src="http://www.40zhe.com/static/jtzdm_lazyload.js?v=wubaiqing2.0"></script>
-		<script type="text/javascript" src="http://www.40zhe.com/static/qwrap_leho.js?v=wubaiqing2.0"></script>
-
-		<script src="http://l.tbcdn.cn/apps/top/x/sdk.js?appkey=21713190"></script>
-
-		<script type="text/javascript">
-			var broadcast_conf = {
-				'goods': ['add', 'delete'],
-				'like': ['add', 'remove'],
-				'follow': ['add', 'remove']
-			};
-			QW.namespace('goods.utils');
-			QW.namespace('goods.widgets');
-			QW.namespace('goods.page');
-			$(document).ready(function(){
-				$(".slideshowlite").slideshow({
-					pauseSeconds: 2,
-					caption: false,
-					width : 980
-				});
-			});
-			$(document).ready(function (){
-				$('#popClose').click(function (){
-					$('#pop').hide();
-				});
-			});
-    (function(win,doc){
-        var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0];
-        if (!win.alimamatk_show) {
-            s.charset = 'gbk';
-            s.async = true;
-            s.src = "http://a.alimama.cn/tkapi.js";
-            s.kslite = "";
-            h.insertBefore(s, h.firstChild);
-        }
-        var o = {
-            pid: "mm_10520376_5110986_15754279",
-            unid:"",
-            rd:1,
-            appkey:"",
-            monitor: function(msg){
-                console.log(msg)
-            },
-            plugins: [
-                //{name: 'keyword'},   /*内文关键字插件*/
-                //{name: 'aroundbox'}  /*任意位置角标插件*/
-            ]
-        }
-        win.alimamatk_onload = win.alimamatk_onload || [];
-        win.alimamatk_onload.push(o);
-    })(window,document);
-
-		</script>
-	</head>
-
-	<body>
-		<?php echo $content; ?>
-		<div style="display:none;">
-			<script src="http://s13.cnzz.com/stat.php?id=5786913&web_id=5786913" language="JavaScript"></script>
-		</div>
-		<script src="http://www.40zhe.com/static/common.js?v=wubaiqing1.5"></script>
-		<script src="http://www.40zhe.com/static/common_40zhe.js?v=wubaiqing1.5"></script>
-	</body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>【美品网】美品网独家优惠,美品网团购,天天特价9.9包邮秒杀在美品网!</title>
+        <meta name="keywords" content="美品网,美品,美品网独家优惠,美品网天天特价，美品网9.9包邮,美品网团购,美品网独家秒杀,meipin">
+        <meta name="description" content="美品网汇集独家折扣商品，9.9包邮宝贝天天有，价格足够低，先到先得。找独家淘宝天猫商城折扣秒杀宝贝，请到美品网。【九块九包邮 天天有】">
+        <meta name="author" content="wubaiqing">
+        <meta name="copyright" content="美品网">
+        <link rel="stylesheet" type="text/css"  href="/assets/main.css" />
+    </head>
+    <body>
+        <div id="header">
+            <div class="index_collection">
+                <div class="top">
+                    <div class="area">
+                        <p onclick="javascript:F_close_shortcut();">
+                            关闭
+                        </p>
+                        按
+                        <strong>
+                            Ctrl+D
+                        </strong>
+                        ，将
+                        <em>
+                            VIP800
+                        </em>
+                        加入收藏夹或者
+                        <a href="http://www.vip800.com/shortcut.php" id="btn_bc" _hover-ignore="1">
+                            下载快捷方式到桌面
+                        </a>
+                        <span>
+                            <a href="javascript:void(0)" onclick="javascript:F_no_shortcut();">
+                                不再提醒
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="topnav">
+                <div class="wp">
+                    <ul class="logininfo r" id="sign">
+                        <li>
+                            您好，欢迎来到金折800！请
+                            <a href="javascript:commonopen();" target="_self">
+                                [登录]
+                            </a>
+                            或
+                            <a href="/user/register.html">
+                                [免费注册]
+                            </a>
+                            <div id="showjs">
+                                <div class="neir">
+                                    <h3>
+                                        <a href="javascript:;" onclick="TINY.box.hide();" target="_self">
+                                        </a>
+                                        用户登录
+                                    </h3>
+                                    <!--form method="post" action="/user/login.html" onsubmit="javascript:return
+                                    F_dlogin_form();"-->
+                                    <form method="post" action="/user/login.html">
+                                        <p>
+                                            <em>
+                                                用户名：
+                                            </em>
+                                            <input type="text" class="text" name="username" id="J_username">
+                                        </p>
+                                        <p>
+                                            <em>
+                                                密 码：
+                                            </em>
+                                            <input type="password" class="text" name="password" id="J_password">
+                                        </p>
+                                        <p id="pvaliCode">
+                                            <em>
+                                                验证码：
+                                            </em>
+                                            <input type="text" class="text code" name="captcha" id="J_captcha">
+                                            <img src="/captcha/index.html" id="valiCode" style="cursor:pointer;" border="0"
+                                            onclick="this.src='/captcha/index.html?'+Math.random();">
+                                        </p>
+                                        <p>
+                                            <em>
+                                                &nbsp;
+                                            </em>
+                                            <input type="submit" value="登 录" class="submit">
+                                            <label for="cookietime">
+                                                <input type="checkbox" name="remember" value="ok" checked="checked" id="J_remember">
+                                                记住我
+                                            </label>
+                                        </p>
+                                    </form>
+                                    <div class="reg">
+                                        没有账号？
+                                        <a href="/user/register.html">
+                                            成为会员
+                                        </a>
+                                        <br>
+                                        <a href="/forget/index.html" target="_blank">
+                                            找回密码
+                                        </a>
+                                        <a href="/oauth/taobao.html" class="tao_login">
+                                            淘宝登录
+                                        </a>
+                                        <a href="/oauth/qq.html" class="Q_login">
+                                            QQ登录
+                                        </a>
+                                        <a href="/oauth/sina.html" class="sina_login">
+                                            新浪微博登录
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="qd">
+                            <a href="javascript:commonopen();" target="_self">
+                                <span>
+                                    签到
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <script language="javascript">
+                        F_sign();
+                    </script>
+                    <ul>
+                        <li class="a">
+                            <a href="javascript:void(0)" onclick="javascript:addToFavorite();" _orighref="javascript:void(0)"
+                                _tkworked="true">
+                                收藏金折
+                            </a>
+                        </li>
+                        <li class="b">
+                            <a href="http://m.vip800.com">
+                                手机版
+                            </a>
+                        </li>
+                        <li style="padding-top:5px;">
+                            <iframe src="http://open.qzone.qq.com/like?url=http%3A%2F%2Fuser.qzone.qq.com%2F2725008382&amp;type=button&amp;width=400&amp;height=30&amp;style=3"
+                                allowtransparency="true" scrolling="no" border="0" frameborder="0" style="width:100px;height:21px;border:none;overflow:hidden;">
+                            </iframe>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="logo_info wp" style="position:relative">
+                <!--script type="text/javascript" src="/static/jz/scripts/secrch.js"></script-->
+                <a href="http://www.vip800.com/mobile" target="_blank" style="position:absolute;right:0;top:0;">
+                    <img src="/data/static/images/appsss.png">
+                </a>
+                <div class="ccc" id="ccc" style="right:260px;">
+                    <!--form action="" method="get" id="ccc_action">				<ul class="ccc_l" id="ccc_ul">					<li class="ccc_li">						<span id="ccc_li"></span>						<span class="ccc_i"></span>					</li>					<li class="ccc_li2"></li>					<li class="ccc_li3" id="ccc_dom"></li>					<li class="ccc_li3 ccc_li5"></li>					<li class="ccc_li4"></li>				</ul>				<div class="ccc_r" id="ccc_r">					<input name="t" id="ccc_t" type="hidden" value="jiukuaijiu" />					<input name="keywords" type="text" placeholder="" search="searchtext" class="text" id="ccc_text" value="" >					<input class="ccc_btn" title="搜索" value="" type="submit">				</div>			</form-->
+                    <script>
+                        function fanli() {
+                            var k = $("#keywords").val();
+                            if (k) {
+                                window.open("http://www.jufanli.com/index.php?mod=tao&act=view&search=1&q=" + k);
+                            }
+                        }
+                        function as() {
+                            var $k = $("#keywords").val();
+                            if ($k == "" || $k == "输入商品名称或者输入淘宝商品地址...") {
+                                alert("请输入商品名称");
+                                return false;
+                            }
+                            return true;
+                        }
+                    </script>
+                    <div id="tmallSearch">
+                        <form action="/search.html" name="searchTop" method="get" onsubmit="return as();">
+                            <div class="mallSearch-input clearfix">
+                                <input type="hidden" name="t" value="all">
+                                <input type="text" name="keywords" id="keywords" class="search_new" onfocus="if(this.value==this.defaultValue)this.value=''"
+                                onblur="if(this.value.length&lt;1)this.value=this.defaultValue;" value="输入商品名称或者输入淘宝商品地址...">
+                                <button type="button" id="jufanli" onclick="fanli();">
+                                    查返利
+                                    <s>
+                                    </s>
+                                </button>
+                                <button type="submit" class="currShopBtn">
+                                    搜商品
+                                    <s>
+                                    </s>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <h1>
+                    <a href="/" title="金折VIP800网，独家秒杀优惠">
+                        金折VIP800网
+                    </a>
+                </h1>
+            </div>
+            <div class="nav">
+                <ul class="wp">
+                    <!--li><a href="/raffle/index.html">幸运抽奖</a></li-->
+                    <!--li><a href="/auction/index.html">疯狂竞拍</a></li-->
+                    <li class="on">
+                        <a href="/">
+                            首页
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/baoyou/">
+                            九块九
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/tttj/">
+                            聚金折
+                        </a>
+                        <em>
+                        </em>
+                    </li>
+                    <!--li><a href="/index/index/type/ershiyuan.html">20元封顶</a></li-->
+                    <!--li><a href="/index/index/type/dujiazhekou.html">独家折扣</a><em></em></li-->
+                    <li>
+                        <a href="/ppzk/">
+                            品牌团
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/smzdm/">
+                            值得买
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://www.jufanli.com/" target="_blank">
+                            聚返利
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/jfsc/">
+                            积分兑换
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/union/">
+                            邀请好友
+                        </a>
+                        <em class="haoyou">
+                        </em>
+                        <!--li><a href="http://jinzhe.uz.taobao.com/" target="_blank">淘宝优站</a></li-->
+                    </li>
+                    <li class="nav_r">
+                        <a href="javascript:;" onclick="biztip();">
+                            卖家中心
+                        </a>
+                        <a href="/help/">
+                            帮助中心
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body>
+    <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000359564'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000359564%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
 </html>
