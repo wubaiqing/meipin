965ba6e3f9758b65c4bf120fa9cede2bff526c31
wubaiqing <wubaiqing@vip.qq.com>
2014-04-09 09:24:34 +0800

    美品头部样式

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -4,7 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>【美品网】美品网独家优惠,美品网团购,天天特价9.9包邮秒杀在美品网!</title>
         <meta name="keywords" content="美品网,美品,美品网独家优惠,美品网天天特价，美品网9.9包邮,美品网团购,美品网独家秒杀,meipin">
-        <meta name="description" content="美品网汇集独家折扣商品，9.9包邮宝贝天天有，价格足够低，先到先得。找独家淘宝天猫商城折扣秒杀宝贝，请到美品网。【九块九包邮 天天有】">
+        <meta name="description" content="">
         <meta name="author" content="wubaiqing">
         <meta name="copyright" content="美品网">
         <link rel="stylesheet" type="text/css"  href="/assets/main.css" />
@@ -42,75 +42,13 @@
                     <ul class="logininfo r" id="sign">
                         <li>
                             您好，欢迎来到金折800！请
-                            <a href="javascript:commonopen();" target="_self">
+                            <a href="" target="_self">
                                 [登录]
                             </a>
                             或
-                            <a href="/user/register.html">
+                            <a href="">
                                 [免费注册]
                             </a>
-                            <div id="showjs">
-                                <div class="neir">
-                                    <h3>
-                                        <a href="javascript:;" onclick="TINY.box.hide();" target="_self">
-                                        </a>
-                                        用户登录
-                                    </h3>
-                                    <!--form method="post" action="/user/login.html" onsubmit="javascript:return
-                                    F_dlogin_form();"-->
-                                    <form method="post" action="/user/login.html">
-                                        <p>
-                                            <em>
-                                                用户名：
-                                            </em>
-                                            <input type="text" class="text" name="username" id="J_username">
-                                        </p>
-                                        <p>
-                                            <em>
-                                                密 码：
-                                            </em>
-                                            <input type="password" class="text" name="password" id="J_password">
-                                        </p>
-                                        <p id="pvaliCode">
-                                            <em>
-                                                验证码：
-                                            </em>
-                                            <input type="text" class="text code" name="captcha" id="J_captcha">
-                                            <img src="/captcha/index.html" id="valiCode" style="cursor:pointer;" border="0"
-                                            onclick="this.src='/captcha/index.html?'+Math.random();">
-                                        </p>
-                                        <p>
-                                            <em>
-                                                &nbsp;
-                                            </em>
-                                            <input type="submit" value="登 录" class="submit">
-                                            <label for="cookietime">
-                                                <input type="checkbox" name="remember" value="ok" checked="checked" id="J_remember">
-                                                记住我
-                                            </label>
-                                        </p>
-                                    </form>
-                                    <div class="reg">
-                                        没有账号？
-                                        <a href="/user/register.html">
-                                            成为会员
-                                        </a>
-                                        <br>
-                                        <a href="/forget/index.html" target="_blank">
-                                            找回密码
-                                        </a>
-                                        <a href="/oauth/taobao.html" class="tao_login">
-                                            淘宝登录
-                                        </a>
-                                        <a href="/oauth/qq.html" class="Q_login">
-                                            QQ登录
-                                        </a>
-                                        <a href="/oauth/sina.html" class="sina_login">
-                                            新浪微博登录
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
                         </li>
                         <li class="qd">
                             <a href="javascript:commonopen();" target="_self">
@@ -120,9 +58,6 @@
                             </a>
                         </li>
                     </ul>
-                    <script language="javascript">
-                        F_sign();
-                    </script>
                     <ul>
                         <li class="a">
                             <a href="javascript:void(0)" onclick="javascript:addToFavorite();" _orighref="javascript:void(0)"
@@ -130,58 +65,20 @@
                                 收藏金折
                             </a>
                         </li>
-                        <li class="b">
-                            <a href="http://m.vip800.com">
-                                手机版
-                            </a>
-                        </li>
-                        <li style="padding-top:5px;">
-                            <iframe src="http://open.qzone.qq.com/like?url=http%3A%2F%2Fuser.qzone.qq.com%2F2725008382&amp;type=button&amp;width=400&amp;height=30&amp;style=3"
-                                allowtransparency="true" scrolling="no" border="0" frameborder="0" style="width:100px;height:21px;border:none;overflow:hidden;">
-                            </iframe>
-                        </li>
                     </ul>
                 </div>
             </div>
             <div class="logo_info wp" style="position:relative">
-                <!--script type="text/javascript" src="/static/jz/scripts/secrch.js"></script-->
                 <a href="http://www.vip800.com/mobile" target="_blank" style="position:absolute;right:0;top:0;">
-                    <img src="/data/static/images/appsss.png">
+                    <img src="/assets/images/appsss.png">
                 </a>
                 <div class="ccc" id="ccc" style="right:260px;">
-                    <!--form action="" method="get" id="ccc_action">				<ul class="ccc_l" id="ccc_ul">					<li class="ccc_li">						<span id="ccc_li"></span>						<span class="ccc_i"></span>					</li>					<li class="ccc_li2"></li>					<li class="ccc_li3" id="ccc_dom"></li>					<li class="ccc_li3 ccc_li5"></li>					<li class="ccc_li4"></li>				</ul>				<div class="ccc_r" id="ccc_r">					<input name="t" id="ccc_t" type="hidden" value="jiukuaijiu" />					<input name="keywords" type="text" placeholder="" search="searchtext" class="text" id="ccc_text" value="" >					<input class="ccc_btn" title="搜索" value="" type="submit">				</div>			</form-->
-                    <script>
-                        function fanli() {
-                            var k = $("#keywords").val();
-                            if (k) {
-                                window.open("http://www.jufanli.com/index.php?mod=tao&act=view&search=1&q=" + k);
-                            }
-                        }
-                        function as() {
-                            var $k = $("#keywords").val();
-                            if ($k == "" || $k == "输入商品名称或者输入淘宝商品地址...") {
-                                alert("请输入商品名称");
-                                return false;
-                            }
-                            return true;
-                        }
-                    </script>
                     <div id="tmallSearch">
-                        <form action="/search.html" name="searchTop" method="get" onsubmit="return as();">
+                        <form action="/search.html" name="searchTop" method="get" >
                             <div class="mallSearch-input clearfix">
                                 <input type="hidden" name="t" value="all">
                                 <input type="text" name="keywords" id="keywords" class="search_new" onfocus="if(this.value==this.defaultValue)this.value=''"
                                 onblur="if(this.value.length&lt;1)this.value=this.defaultValue;" value="输入商品名称或者输入淘宝商品地址...">
-                                <button type="button" id="jufanli" onclick="fanli();">
-                                    查返利
-                                    <s>
-                                    </s>
-                                </button>
-                                <button type="submit" class="currShopBtn">
-                                    搜商品
-                                    <s>
-                                    </s>
-                                </button>
                             </div>
                         </form>
                     </div>
@@ -194,54 +91,15 @@
             </div>
             <div class="nav">
                 <ul class="wp">
-                    <!--li><a href="/raffle/index.html">幸运抽奖</a></li-->
-                    <!--li><a href="/auction/index.html">疯狂竞拍</a></li-->
                     <li class="on">
                         <a href="/">
                             首页
                         </a>
                     </li>
                     <li>
-                        <a href="/baoyou/">
-                            九块九
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/tttj/">
-                            聚金折
-                        </a>
-                        <em>
-                        </em>
-                    </li>
-                    <!--li><a href="/index/index/type/ershiyuan.html">20元封顶</a></li-->
-                    <!--li><a href="/index/index/type/dujiazhekou.html">独家折扣</a><em></em></li-->
-                    <li>
-                        <a href="/ppzk/">
-                            品牌团
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/smzdm/">
-                            值得买
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://www.jufanli.com/" target="_blank">
-                            聚返利
-                        </a>
-                    </li>
-                    <li>
                         <a href="/jfsc/">
                             积分兑换
                         </a>
-                    </li>
-                    <li>
-                        <a href="/union/">
-                            邀请好友
-                        </a>
-                        <em class="haoyou">
-                        </em>
-                        <!--li><a href="http://jinzhe.uz.taobao.com/" target="_blank">淘宝优站</a></li-->
                     </li>
                     <li class="nav_r">
                         <a href="javascript:;" onclick="biztip();">
@@ -254,6 +112,32 @@
                 </ul>
             </div>
         </div>
+
+
+
+
+
+
+
+
+        <div class="side_nav">  <div class="logo"><a href="http://club.jinzhe.com/"><img src="/data/static/images/side_logo.png" alt="金折会旗下"></a></div>  <div class="content">    <div class="hd">      <h3><a href="http://www.vip800.com/">金折网</a>精选</h3>    </div>    <div class="bd">	<ul>        <li><a href="/nvzhuang">女装</a></li>        <li><a href="/nanzhuang">男装</a></li>        <li><a href="/home">居家</a></li>        <li><a href="/muying">母婴</a></li>        <li><a href="/xiangbaoxiexue">鞋包</a></li>        <li><a href="/baihuo">配饰</a></li>        <li><a href="/shipinjiushui">美食</a></li>        <li><a href="/bodycare">化妆</a></li>	   <li><a href="/sport">文体</a></li>		<li><a href="/shumajiadian">数码</a></li>      </ul>	     </div>    <div class="hd">      <h3><a href="http://www.vip800.com">金折网</a>推荐</h3>    </div>    <div class="bd">      <ul>		<li><a href="http://www.vip800.com/baoyou/" _hover-ignore="1">九块邮</a></li>		<li><a href="http://www.vip800.com/tttj/">聚金折</a></li>        <li><a href="http://www.vip800.com/smzdm">值得买</a></li>		<li><a href="http://www.vip800.com/ppzk/">品牌团</a></li>       </ul>    </div>    <div class="hd">      <h3>下载金<a href="http://www.vip800.com/">折800</a></h3>    </div>    <div class="bd">      <div class="qrcode"><img src="/data/static/images/weicode.png">        <p>下载手机客户端</p>      </div>    </div>  </div>  <div class="bottom"><a href="#" id="goTop"><i class="icon-top"></i>TOP</a></div>  <script>		$(function(){		  $(window).scroll(function() {			if($(window).scrollTop()>=250){				$(".side_nav").addClass("affix");			}else{				$(".side_nav").removeClass("affix");			}		  });		});  </script></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </body>
     <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000359564'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000359564%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
 </html>
+<strong>
