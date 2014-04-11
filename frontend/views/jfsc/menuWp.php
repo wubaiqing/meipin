03af9bcebd7ae2ec3fe17dbf26dbad5826376201
liyongwei <317180396@qq.com>
2014-04-11 15:15:56 +0800

    积分商城文件

---
+++ frontend/views/jfsc/menuWp.php
@@ -0,0 +1,13 @@
+<div class="menu_wp">
+    <div class="junav">
+        <span>全部折扣<em>(1091)</em></span>
+        <div class="new_bg r">
+            <ul>
+                <li class="on"><a href="/">全站新品<em>(14)</em></a></li>
+                <li><a href="/yugao.html">明日精选<em>(0)</em></a></li>
+                <!--li ><a href="/index/index/order/hot.html">最热</a></li-->
+            </ul>
+        </div>
+        <h5>小编会从早9点到晚21点，每整点更新一次给力宝贝！</h5>
+    </div>
+</div>
---
+++ frontend/views/jfsc/prompt.php
@@ -0,0 +1,26 @@
+<div class="index_collection">
+    <div class="top">
+        <div class="area">
+            <p onclick="">
+                关闭
+            </p>
+            按
+            <strong>
+                Ctrl+D
+            </strong>
+            ，将
+            <em>
+                美品网
+            </em>
+            加入收藏夹或者
+            <a href="http://www.meipin.com/" id="btn_bc" _hover-ignore="1">
+                下载快捷方式到桌面
+            </a>
+            <span>
+                <a href="javascript:void(0)" onclick="">
+                    不再提醒
+                </a>
+            </span>
+        </div>
+    </div>
+</div>
