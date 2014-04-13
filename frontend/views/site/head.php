d12603ccf13e93586b9001769e67e91cb4a4a8b4
wubaiqing <wubaiqing@vip.qq.com>
2014-04-13 22:30:32 +0800

    修改搜索按钮

--- frontend/views/site/head.php
+++ frontend/views/site/head.php
@@ -9,7 +9,7 @@
                     <input type="hidden" name="t" value="all">
                     <input type="text" name="keywords" id="keywords" class="search_new" onfocus="if(this.value==this.defaultValue)this.value=''"
                     onblur="if(this.value.length&lt;1)this.value=this.defaultValue;" value="输入商品名称或者输入淘宝商品地址...">
-                    <button type="button" id="jufanli" onclick="fanli();">查返利<s></s></button>
+                    <button type="button" id="search"></button>
                 </div>
             </form>
         </div>
