9c63839f5bf245a9dcd53386a02ed67a023c188d
wubaiqing <wubaiqing@vip.qq.com>
2014-04-13 22:01:18 +0800

    修改美品注释信息

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -1,23 +1,19 @@
 <?php
 /**
- * 今天值得买首页
+ * 美品网首页
  * @author wubaiqing<wubaiqing@vip.qq.com>
- * @copyright Copyright (c) 2013 今天值得买
- * @since 1.5
+ * @copyright Copyright (c) 2014 美品
+ * @since 1.0
  */
 class SiteController extends Controller
 {
-
     /**
      * @var string $layout 继承视图
      */
-    public $layout = '//layouts/main';
+    protected $layout = '//layouts/main';
 
     /**
      * 今天值得买首页
-     * @param integer $cat  当前分类
-     * @param intger  $page 当前页数
-     * @param integer $hot  0最热, 1热门
      */
     public function actionIndex($cat = 0, $page = 1, $hot = 0)
     {
