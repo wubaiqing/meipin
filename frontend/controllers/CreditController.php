8e6f3cf7bf7a8eee0032078ffa651a8cbc0460c1
liyongwei <317180396@qq.com>
2014-04-16 16:22:11 +0800

    修改积分商城文件名称

---
+++ frontend/controllers/CreditController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 今天值得买首页
+ * @author wubaiqing<wubaiqing@vip.qq.com>
+ * @copyright Copyright (c) 2013 今天值得买
+ * @since 1.5
+ */
+class CreditController extends Controller
+{
+
+    /**
+     * @var string $layout 继承视图
+     */
+    public $layout = '//layouts/main';
+
+    /**
+     * 今天值得买首页
+     * @param integer $cat  当前分类
+     * @param intger  $page 当前页数
+     * @param integer $hot  0最热, 1热门
+     */
+    public function actionIndex($cat = 0, $page = 1, $hot = 0)
+    {
+		
+        $this->render('index', array());
+    }
+
+}
---
+++ frontend/views/credit/nav.php
@@ -0,0 +1,22 @@
+<div class="nav">
+    <ul class="wp">
+        <li class="on">
+            <a href="/">
+                首页
+            </a>
+        </li>
+        <li>
+            <a href="/jfsc/">
+                积分兑换
+            </a>
+        </li>
+        <li class="nav_r">
+            <a href="javascript:;" onclick="biztip();">
+                卖家中心
+            </a>
+            <a href="/help/">
+                帮助中心
+            </a>
+        </li>
+    </ul>
+</div>
