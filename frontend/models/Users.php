e1c55cc5d9aa074a663e7a21666bc2ea689532a2
wubaiqing <wubaiqing@vip.qq.com>
2014-04-24 23:36:25 +0800

    用户model

---
+++ frontend/models/Users.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 商品管理
+ * @author wubaiqing<wubaiqing@vip.qq.com>
+ * @copyright Copyright (c) 2013 今天值得买
+ * @since 1.5
+ */
+class Users extends ActiveRecord implements IArrayable
+{
+	/**
+	 * 表名
+	 * @return string
+	 */
+    public function tableName()
+    {
+        return '{{meipin_users}}';
+    }
+
+    /**
+     * 验证规则
+     * @return array
+     */
+    public function rules()
+    {
+        return array(
+            array('id, username, password, salt, created_at, updated_at', 'safe'),
+        );
+    }
+}
+
