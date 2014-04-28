371914375d6cf64b6c15c161f10999b6a66131c0
wubaiqing <wubaiqing@vip.qq.com>
2014-04-28 23:51:06 +0800

    更新js样式

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -9,7 +9,7 @@
         <meta name="copyright" content="美品网">
         <link rel="stylesheet" type="text/css"  href="/assets/main.css?v=wubaiqing" />
         <script type="text/javascript" src="/assets/js/78df5a3f36d83192e43966bc05d643b2.js?v=1.0.1"></script>
-        <script type="text/javascript" src="/assets/js/tuanpub3.0.min.js?v=1.0.1"></script>
+        <script type="text/javascript" src="/assets/js/tuanpub3.0.min.js?v=1.0.1.0"></script>
     </head>
     <body>
         <?php echo $content;?>
