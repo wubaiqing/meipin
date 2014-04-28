8032893f4d7154d2d4cfbc643699d14891fc499a
wubaiqing <wubaiqing@vip.qq.com>
2014-04-28 18:08:19 +0800

    美品最新最热

--- frontend/views/site/menuWp.php
+++ frontend/views/site/menuWp.php
@@ -5,8 +5,7 @@
         <div class="new_bg r">
             <ul>
                 <li class="on"><a href="/">最热<em></em></a></li>
-                <li><a href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 0, 'hot' => 1));?>">最新<em></em></a></li>
-                <!--li ><a href="/index/index/order/hot.html">最热</a></li-->
+                <li class="<?php $hot = Yii::app()->request->getQuery('hot'); ?>"><a href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 0, 'hot' => 1));?>">最新<em></em></a></li>
             </ul>
         </div>
         <h5>小编会从早9点到晚21点，每整点更新一次给力宝贝！</h5>
