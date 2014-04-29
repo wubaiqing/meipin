aeef50e9a5e3fae8635ff99dc016d240b89db9ed
wubaiqing <wubaiqing@vip.qq.com>
2014-04-29 17:56:38 +0800

    优化title,description,keyword

---
+++ frontend/controllers/SearchController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * 美品网首页
+ * @author wubaiqing<wubaiqing@vip.qq.com>
+ * @copyright Copyright (c) 2014 美品
+ * @since 1.0
+ */
+class SearchController extends Controller
+{
+    /**
+     * @var string $layout 继承视图
+     */
+    public $layout = '//layouts/main';
+
+    /**
+     * 今天值得买首页
+     */
+    public function actionIndex($page = 1, $hot = 0, $cat = 0)
+    {
+        $goods = Goods::getGoodsList($cat, $hot, $page);
+        $this->render('index', array(
+            'goods' => $goods['data'],
+            'pager' => $goods['pager'],
+        ));
+    }
+
+    /**
+     * 商品搜索
+     */
+    public function actionSearch($title)
+    {
+        $cacheKey = 'index-search-'.md5($title);
+        $goods = Yii::app()->cache->get($cacheKey);
+        if (empty($result)) {
+            $title = trim($title);
+            $criteria = new CDbCriteria();
+            $criteria->addSearchCondition('title', $title);
+            $count = Goods::model()->count($criteria);
+            $pages = new CPagination($count);
+            $pages->pageSize=15;
+            $pages->applyLimit($criteria);
+            $goods = Goods::model()->findAll($criteria);
+            Yii::app()->cache->set($cacheKey, $goods, 3600);
+        }
+
+        if ($goods) {
+            $this->render('search', array(
+                'goods' => $goods,
+                'pager' => new CPagination($count)
+            ));
+        } else {
+            $this->render('searchError', array(
+                'title' => $title
+            ));
+        }
+    }
+
+    /**
+     * 今天值得买首页
+     */
+    public function actionOut($id)
+    {
+        $goodsId = Des::decrypt($id);
+        $goods = Goods::getGoods($goodsId);
+        if (!empty($goods)) {
+            header("Location:{$goods->url}");
+            Yii::app()->end();
+        }
+    }
+
+}
+
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>【美品网】美品网独家优惠,美品网团购,天天特价9.9包邮秒杀在美品网!</title>
-        <meta name="keywords" content="美品网,美品,美品网独家优惠,美品网天天特价，美品网9.9包邮,美品网团购,美品网独家秒杀,meipin">
+        <title><?php echo Yii::app()->params['title'];?></title>
+        <meta name="keywords" content="<?php echo Yii::app()->params['keyword'];?>">
         <meta name="description" content="">
         <meta name="author" content="wubaiqing">
         <meta name="copyright" content="美品网">
