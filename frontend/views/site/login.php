054b7941d3702ebe07d060f77ad2239c7cd021e2
wubaiqing <wubaiqing@vip.qq.com>
2014-05-04 23:11:53 +0800

    头部个人中心

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -3,7 +3,16 @@
         <ul class="logininfo r" id="sign">
             <?php if (Yii::app()->user->id) {?>
             <li>
-                欢迎您，<a href="javascript:void(0);"><?php echo Yii::app()->user->name;?></a><a href="javascript:void(0);" class="level v0"></a><span class="nav_split">|<a title="退出" class="lnk_logout" href="<?php echo $this->createUrl('user/Logout');?>">退出</a><span class="nav_split">|</span>
+                欢迎您，
+                <a href="javascript:void(0);">
+                    <?php echo Yii::app()->user->name;?>
+                </a>
+                <a href="javascript:void(0);" class="level v0"></a>
+                <span class="nav_split">|</span>
+                <a href="<?php echo $this->createUrl('user/index');?>">个人中心</a>
+                <span class="nav_split">|</span>
+                <a title="退出" class="lnk_logout" href="<?php echo $this->createUrl('user/logout');?>">退出</a>
+                <span class="nav_split">|</span>
             </li>
             <?php } else {?>
             <li>
