05e6a14abcc278606965a11cb1c6a1b4f182ff54
pangaofeng <pan@q.com>
2014-05-06 07:26:06 +0800

    积分样式添加

---
+++ frontend/models/Score.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 美品网商城数据
+ * @author wubaiqing <wubaiqing@vip.qq.com>
+ * @copyright Copyright (c) 2014 美品网
+ * @since 1.0
+ */
+class Score extends ActiveRecord implements IArrayable
+{
+    /**
+     * 表名
+     * @return string
+     */
+    public function tableName()
+    {
+        return '{{meipin_score}}';
+    }
+
+    /**
+     * 获取商城名称
+     * @param  integer $id 商城ID
+     * @return string  商城名称
+     */
+    public static function getStoreByPk($id)
+    {
+        // 缓存名称
+        $cacheKey = 'meipin-score-'.$id;
+
+        // 得到缓存数据
+        $name = Yii::app()->cache->get($cacheKey);
+        if (!empty($name)) {
+            return $name;
+        }
+
+        $array = [];
+        $storeAll = self::model()->findAll();
+        foreach ($storeAll as $val) {
+            $array[$val->id] = $val->name;
+        }
+
+        if (!isset($array[$id])) {
+            $name = '淘宝网';
+        } else {
+            $name = $array[$id];
+        }
+
+        Yii::app()->cache->set($cacheKey, $name, 86400);
+
+        return $name;
+    }
+}
---
+++ frontend/views/user/left.php
@@ -0,0 +1,22 @@
+<div class="user_l l">
+    <h3>用户中心</h3>
+    <div class="bor">
+        <h4><span class="a">我的礼品</span></h4>
+        <ul>
+            <li><a href="/user/order">我的订单</a></li>
+            <li><a href="/user/trace">我的浏览记录</a></li>
+            <li><a href="/user/score">我的积分</a></li>
+            <li><a href="/user/gift">我的礼品</a></li>
+            <li><a href="/user/friend">我邀请的朋友</a></li>
+        </ul>
+        <h4><span class="c">个人信息</span></h4>
+        <ul>
+            <li><a href="/user/info">基本信息</a></li>
+            <li><a href="/user/oauth">关联账号</a></li>
+            <li><a href="/user/address">收货地址</a></li>
+            <li><a href="/user/password">修改密码</a></li>
+        </ul>
+        <div class="hr"></div>
+    </div>
+
+</div>
