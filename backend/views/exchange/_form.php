c8ceec59f07c4fa7d633e8e19d767c0a8bbc67ca
zhangchao <zhangchao@55tuan.com>
2014-05-07 16:49:20 +0800

    积分兑换功能v1

---
+++ backend/views/exchange/_form.php
@@ -0,0 +1,115 @@
+<?php
+// 去掉必填项kk
+CHtml::$afterRequiredLabel = '';
+$form=$this->beginWidget('CActiveForm', array(
+    'id' => 'goods-form',
+    'method'=>'post',
+    'enableAjaxValidation' => false,
+    'htmlOptions' => array(
+        'class' => 'form-horizontal',
+    ),
+));
+CHtml::$errorSummaryCss = 'text-warning';
+
+?>
+    <?php echo $form->errorSummary($exchangeModel); ?>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'name', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'name');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'url_name', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'url_name');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'num', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'num');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'price', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'price');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'integral', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'integral');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'start_time', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'start_time');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'end_time', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'end_time');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'need_level', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'need_level');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'taobao_id', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'taobao_id');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'detail_url', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->urlField($exchangeModel,'detail_url');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'taobaoke_url', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->urlField($exchangeModel,'taobaoke_url');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'taobaoke_shop_url', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->urlField($exchangeModel,'taobaoke_shop_url');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'support_name', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textField($exchangeModel,'support_name');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'support_url', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->urlField($exchangeModel,'support_url');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'description', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->textArea($exchangeModel,'description');?>
+        </div>
+</div>
+<div class="control-group">
+        <?php echo $form->labelEx($exchangeModel,'img_url', array('class' => 'control-label')); ?>
+        <div class="controls">
+        <?php echo $form->fileField($exchangeModel,'img_url');?>
+        </div>
+</div>
+<div class="form-actions">
+        <?php echo CHtml::submitButton($exchangeModel->isNewRecord ? '添加' : '修改', array('class' => 'btn btn-primary save')); ?>
+    </div>
+<?php $this->endWidget(); ?>
---
+++ backend/views/exchange/create.php
@@ -0,0 +1,4 @@
+<div class="box">
+    <h3 class="box-header">添加积分兑换</h3>
+    <?php $this->renderPartial('_form', array('exchangeModel'=>$exchangeModel)); ?>
+</div>
