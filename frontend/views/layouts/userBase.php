63bb7f4fc25ee7ad9917cf896212124a8aea5f07
wubaiqing <wubaiqing@vip.qq.com>
2014-05-08 18:12:31 +0800

    修改用户js文件

--- frontend/views/layouts/userBase.php
+++ frontend/views/layouts/userBase.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" type="text/css"  href="/assets/main.css?v=201404131000" />
         <link rel="stylesheet" type="text/css"  href="/assets/user.css?v=201404131000" />
         <script src="http://libs.baidu.com/jquery/1.9.0/jquery.js"></script>
-        <script type="text/javascript" src="/assets/js/user.js?v=1.0.1"></script>
+        <script type="text/javascript" src="/assets/js/user.js?v=1.0.2"></script>
     </head>
     <body>
         <div id="header">
--- frontend/www/assets/js/user.js
+++ frontend/www/assets/js/user.js
@@ -8,11 +8,13 @@ User.Address = (function () {
         'changeProvince' : function () {
             var url = $('#getProvinceUrl').val();
             $('#userProvince').change(function () {
+                $('#userCity').attr('disabled', 'disabled');
                 $.getJSON(url, {'provinceId' : $(this).val()}, function (json) {
                     if (json.status == 0) {
                         return false;
                     }
                     $('#userCity').html(json.data);
+                    $('#userCity').removeAttr('disabled');
                 });
             });
         }
