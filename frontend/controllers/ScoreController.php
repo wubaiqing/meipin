3e91c834dea03f74edf2f1f289e0cadd964a8a29
wubaiqing <wubaiqing@vip.qq.com>
2014-05-13 10:15:00 +0800

    解决积分乱码

--- frontend/controllers/ScoreController.php
+++ frontend/controllers/ScoreController.php
@@ -1,66 +1,52 @@
 <?php
-
 /**
- * ��ֲ���������
+ * 积分管理
  * @author liukui<liujickson@gmail.com>
  * @copyright Copyright (c) 2014 ��Ʒ
  * @since 1.0
  */
 class ScoreController extends Controller
 {
-
     /**
-     * @var string $layout �̳���ͼ
+     * @var string $layout
      */
     public $layout = '//layouts/main';
 
     /**
-     * ���ҵ������
-     * @var ScoreService 
+     * @var ScoreService 积分服务
      */
     public $scoreService;
 
     public function init()
     {
         parent::init();
-
         $this->scoreService = new ScoreService();
     }
 
     /**
-     * ��ֶһ���ҳ
-    /**
-     * ��ֹ���-�����ϸ�б�
+     * 积分首页
      */
     public function actionIndex()
     {
-        // �û�ID
         $userId = Yii::app()->user->id;
         $layout = '//layouts/user';
-        // ��ȡ�û���¼
         $user = User::getUser($userId);
-        // ��ȡ�û���ּ�¼
         $score = Score::getScoreByUserId($userId);
-        //var_dump($score);
         $this->render('index', [
             'user' => $user,
             'score' => $score
         ]);
     }
 
-
     /**
-     * ��ֹ���-��������б�
+     * 积分列表ajax切换
      */
     public function actionAjax()
     {
-        // �û�ID
         $userId = Yii::app()->user->id;
         $type = $_POST['type'];
-        // ��ȡ�û���¼
         $user = User::getUser($userId);
-        // ��ȡ�û���ּ�¼
         $score = Score::getScoreByUserId($userId,$type);
-        //var_dump(json_encode($score));
         return json_encode($score);
     }
+}
