4a3ab1c96c0728c251e98e007ad1d99514e5267e
wubaiqing <wubaiqing@vip.qq.com>
2014-05-14 13:39:45 +0800

    优化积分

--- frontend/models/Score.php
+++ frontend/models/Score.php
@@ -29,10 +29,10 @@ class Score extends ActiveRecord implements IArrayable
         // 得到缓存数据
         $name = Yii::app()->cache->get($cacheKey);
         if (!empty($name)) {
-            //return $name;
+            return $name;
         }
 
-		$scoreList = [];
+        $scoreList = [];
         $scorePaginate = Score::model()->dataList($user_id, $type)->paginate(null,$page);
         $scoreList['pager'] = $scorePaginate->getPagination();
         $scoreList['data'] = $scorePaginate->data;
@@ -42,10 +42,10 @@ class Score extends ActiveRecord implements IArrayable
             'pager' => $scoreList['pager'],
             'data' => $scoreList['data']
         ], 1800);
-		//var_dump($scoreList);exit;
+
         return $scoreList;
     }
-    
+
 
     /**
      * 数据SQL条件
@@ -61,31 +61,30 @@ class Score extends ActiveRecord implements IArrayable
         $criteria->compare('user_id', '=' . $user_id);
         if($type=='add')
         {
-        	$criteria->compare('score', '>=0');
+            $criteria->compare('score', '>=0');
         }
         elseif($type=='reduce')
         {
-        	$criteria->compare('score', '<0');
+            $criteria->compare('score', '<0');
         }
-
         $this->dbCriteria->mergeWith($criteria);
 
         return $this;
     } 
-    
+
     /**
      * 
      * */
-     public function getScoreTitle($type_id)
-     {
-     	$type_list = [
-     					1=>'签到增加',
-     					2=>'连续签到2天增加',
-     					3=>'连续签到3天增加',
-     					4=>'连续签到3天以上增加',
-     					5=>'商品兑换',
-     					6=>'积分兑换',
-     				];
-     	return $type_list[$type_id];
-     }   
+    public static function getScoreTitle($type_id)
+    {
+        $type_list = [
+            1=>'签到增加',
+            2=>'连续签到2天增加',
+            3=>'连续签到3天增加',
+            4=>'连续签到3天以上增加',
+            5=>'商品兑换',
+            6=>'积分兑换',
+        ];
+        return $type_list[$type_id];
+    }   
 }
