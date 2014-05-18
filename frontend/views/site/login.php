73e59e69506b16693b305919e7e7ead3233eec8c
liukui <liukui@55tuan.com>
2014-05-18 18:42:25 +0800

    udpate

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -20,10 +20,15 @@
                     <a href="<?php echo $this->createUrl('user/login'); ?>" target="_self">[登录]</a>或<a href="<?php echo $this->createUrl('user/register'); ?>">[免费注册]</a>
                 </li>
             <?php } ?>
-            <?php if (!empty($this->user)): ?>
+            
                 <li class="qd <?php echo!empty($this->user) && (date("Y-m-d", $this->user->last_dr_time) == date("Y-m-d", time())) ? 'qd_ok' : ''; ?>">
-                    <span href="javascript:;" onmouseover="document.getElementById('con_qd').style.display = 'block'" onmouseout="document.getElementById('con_qd').style.display = 'none'"class='qiandao'>&nbsp;
+                    <span href="javascript:;" 
+                          <?php if (!empty($this->user)): ?>
+                          onmouseover="document.getElementById('con_qd').style.display = 'block'" onmouseout="document.getElementById('con_qd').style.display = 'none'" 
+                          <?php endif; ?>
+                          class='qiandao'>&nbsp;
                         <div id="con_qd" style="">
+                        <?php if (!empty($this->user)): ?>
                             <em>连签：<strong class="big" id="dr_count"><?php echo $this->user->dr_count ?></strong> 天，积分<strong id="nowScore">
                                     +<?php
                                     $scoreList = Yii::app()->params['dayRegistionNum'];
@@ -31,15 +36,16 @@
                                     ?>
                                 </strong></em>
                             <em>明日签到可得<strong id="nextCount"><?php echo ($this->user->dr_count) > 3 ? 3 : ($scoreList[$this->user->dr_count] + 1); ?></strong>积分 </em>
+                        <?php endif; ?>
                         </div>
-
                     </span>
                 </li>
-            <?php endif; ?>
+            
         </ul>
         <ul>
             <li class="a">
             </li>
         </ul>
     </div>
+    <?php echo CHtml::hiddenField('unlogin_url',Yii::app()->createAbsoluteUrl("user/login"));?>
 </div>
