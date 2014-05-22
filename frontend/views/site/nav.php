f3cac95b45f102e9444c0d6af8e5c9555ee07396
wubaiqing <wubaiqing@vip.qq.com>
2014-05-22 22:02:10 +0800

    积分兑换头部

--- frontend/views/site/nav.php
+++ frontend/views/site/nav.php
@@ -1,10 +1,10 @@
 <div id="head_nav">
     <div class="head_nav" id="t-area">
 	<div class="l">
-	    <a class="on" href="http://www.meipin.com">首页<i></i></a>
-	    <a href="<?php echo $this->createUrl('site/index', array('cat' => 1000));?>">九块九包邮<i></i></a>
-	    <a href="<?php echo $this->createUrl('site/index', array('cat' => 1001));?>">聚美品<i></i></a>
-	    <a target="_blank" href="<?php echo $this->createUrl('exchange/index');?>" class="<?php echo (Yii::app()->controller->id == 'exchange') ? 'on' : ''; ?>">积分兑换</a>
+	    <a class="<?php echo (Yii::app()->controller->id == 'site' && $cat < 1000) ? 'on' : ''; ?>" href="http://www.meipin.com">首页<i></i></a>
+	    <a class="<?php echo (Yii::app()->controller->id == 'site' && $cat == 1000) ? 'on' : ''; ?>" href="<?php echo $this->createUrl('site/index', array('cat' => 1000));?>">九块九包邮<i></i></a>
+	    <a class="<?php echo (Yii::app()->controller->id == 'site' && $cat == 1001) ? 'on' : ''; ?>" href="<?php echo $this->createUrl('site/index', array('cat' => 1001));?>">聚美品<i></i></a>
+	    <a href="<?php echo $this->createUrl('exchange/index');?>" class="<?php echo (Yii::app()->controller->id == 'exchange') ? 'on' : ''; ?>">积分兑换</a>
 	    <span class="n"></span>
 	</div>
 	<div class="r_con">
