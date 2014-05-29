62d667ce6f97442920b5b0cd8ca940044e6136bc
wubaiqing <wubaiqing@vip.qq.com>
2014-05-29 11:06:27 +0800

    添加gitignore

---
+++ frontend/www/static/js/score/score.js
@@ -0,0 +1,53 @@
+/* 代码整理：懒人之家 lanrenzhijia.com */
+var columnReadyCounter = 0;
+
+// This is the callback function for the "hiding" animations
+// it gets called for each animation (since we don't know which is slowest)
+// the third time it's called, then it resets the column positions
+
+// When the DOM is ready
+$(function() {
+
+    var $allContentBoxes = $(".content-box"),
+        $allTabs = $(".tabs_score"),
+        $el,hrefSelector = "";
+
+
+    // first tab and first content box are default current
+    $(".content-box:first").addClass("score_current");
+    $(".box-wrapper .current .col").css("top", 0);
+
+    $("#score").delegate(".tabs_score", "click", function() {
+//
+//        $el = $(this);
+//
+//        if ( (!$el.hasClass("current")) && ($(":animated").length == 0 ) ) {
+//
+//            // current tab correctly set
+//            $allTabs.removeClass("current");
+//            $el.addClass("current");
+//            hrefSelector = $el.attr("href");
+//            var type = hrefSelector.replace('#','');
+//            var page=1;
+//            $.ajax({
+//                url:"/score/ajax",
+//                data:{"type":type,"page":page},
+//                type:"POST",
+//                dataType:"json",
+//                success:function(data){
+//                    var str = "";
+//                },
+//                error : function(data){
+//                return false;
+//            }
+//            })
+//            // new content box is marked as current
+//            $allContentBoxes.removeClass("score_current");
+//            $(hrefSelector).addClass("score_current");
+//
+//
+//        };
+//
+//    });
+
+});
---
+++ frontend/www/static/js/sign_day.js
@@ -0,0 +1,33 @@
+/*积分签到*/
+$(function() {
+    $('.qiandao').click(function(){
+        var input = this;
+        var signApi = '/user/DayRegistion';
+        if($(input).hasClass("signed")){
+            return false;
+        }
+        $.get(signApi, {}, function(result) {
+            if (result.status == false) {
+                //未登录
+                if (!result.data.isLogin) {
+                    window.location.href = $('#unlogin_url').val();
+                }else{
+                    alert(result.data.message);
+                }
+                return false;
+            } else {
+                alert(result.data.message);
+                // $('.qiandao').removeClass('unsign').addClass('signed').removeClass('qiandao');
+                $('#jryq').html(result.data.message2);
+                //成功提示
+                $("#dr_count").html(result.data.dr_count);
+                $("#nowScore").html(result.data.score);
+                $("#nextCount").html(result.data.nextScore);
+                return true;
+            }
+        }, 'json');
+    });
+    $('.cheng').click(function(){
+        alert("您已经签过了");
+    });
+});
