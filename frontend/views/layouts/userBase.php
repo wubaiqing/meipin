19271b73763e393fd7e6811c89ad052b780a15bb
wubaiqing <wubaiqing@vip.qq.com>
2014-05-29 11:03:30 +0800

    删除assets，修改assets目录为static

--- frontend/views/layouts/userBase.php
+++ frontend/views/layouts/userBase.php
@@ -7,11 +7,11 @@
         <meta name="description" content="">
         <meta name="author" content="wubaiqing">
         <meta name="copyright" content="美品网">
-        <link rel="stylesheet" type="text/css"  href="/assets/main.css?v=201404131000" />
-        <link rel="stylesheet" type="text/css"  href="/assets/user.css?v=201404131000" />
+        <link rel="stylesheet" type="text/css"  href="/static/main.css?v=201404131000" />
+        <link rel="stylesheet" type="text/css"  href="/static/user.css?v=201404131000" />
         <script src="http://libs.baidu.com/jquery/1.9.0/jquery.js"></script>
-        <script type="text/javascript" src="/assets/js/user.js?v=1.0.2"></script>
-        <script type="text/javascript" src="/assets/js/sign_day.js?v=1.0.1"></script>
+        <script type="text/javascript" src="/static/js/user.js?v=1.0.2"></script>
+        <script type="text/javascript" src="/static/js/sign_day.js?v=1.0.1"></script>
     </head>
     <body>
         <div id="header">
