43350a846496ef11a473dae1538e7b608489323d
909933794 <909933794@11.com>
2014-05-30 16:33:27 +0800

    去掉全部折扣右面的图片

--- frontend/views/site/nav.php
+++ frontend/views/site/nav.php
@@ -1,7 +1,7 @@
 <div id="head_nav">
     <div class="head_nav" id="t-area">
     <div class="l">
-        <a class="<?php echo (Yii::app()->controller->id == 'site' && $cat < 1000) ? 'on' : ''; ?>" href="http://www.meipin.com">首页<i></i></a>
+        <a class="<?php echo (Yii::app()->controller->id == 'site' && $cat < 1000) ? 'on' : ''; ?>" href="/">首页<i></i></a>
         <a class="<?php echo (Yii::app()->controller->id == 'site' && $cat == 1000) ? 'on' : ''; ?>" href="<?php echo $this->createUrl('site/index', array('cat' => 1000));?>">九块九包邮<i></i></a>
         <a class="<?php echo (Yii::app()->controller->id == 'site' && $cat == 1001) ? 'on' : ''; ?>" href="<?php echo $this->createUrl('site/index', array('cat' => 1001));?>">聚美品<i></i></a>
         <a href="<?php echo $this->createUrl('exchange/index');?>" class="<?php echo (Yii::app()->controller->id == 'exchange') ? 'on' : ''; ?>">积分兑换</a>
