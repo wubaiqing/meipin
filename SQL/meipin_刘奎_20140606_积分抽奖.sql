e07602a88885d742a641c7420a69e7b9aa22befb
liukui <liukui@55tuan.com>
2014-06-08 13:26:14 +0800

    抽奖首页

--- SQL/meipin_刘奎_20140606_积分抽奖.sql
+++ SQL/meipin_刘奎_20140606_积分抽奖.sql
@@ -1,2 +1,3 @@
 -- 2014-06-06 增加抽奖商品类型
 alter table `meipin_exchange` add goods_type tinyint(4) DEFAULT NULL COMMENT '商品类型，0:积分兑换商品；1:抽奖商品';
+alter table `meipin_exchange` add limit_count tinyint(4) DEFAULT 1 COMMENT '中奖名额';
