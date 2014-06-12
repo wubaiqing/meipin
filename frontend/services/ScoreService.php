7ccd614e4eec5198bb6da6902056888010928c45
liukui <liukui@55tuan.com>
2014-06-12 21:51:31 +0800

    update

--- frontend/services/ScoreService.php
+++ frontend/services/ScoreService.php
@@ -123,7 +123,7 @@ class ScoreService
         if ($user->score < $goods->integral) {
             return CommonHelper::getDataResult(false, [
                         'message' => "真遗憾！您只有" . $user->score . "积分,不足以兑换此商品,您可以到每天签到，领取更多积分",
-                        'url' => $url
+                        'url' => $indexUrl
             ]);
         }
         if ($user->mobile_bind == 0) {
