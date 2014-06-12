81601280fd1e9fb2777ab0b704ebecdce82e0f71
wubaiqing <wubaiqing@vip.qq.com>
2014-06-12 15:59:25 +0800

    修改美品图片

--- frontend/views/site/banner.php
+++ frontend/views/site/banner.php
@@ -2,9 +2,9 @@
     <div class="fi07_1">
         <ul style="width: 3920px;">
 	    <li class="current" data-id="0"><img src="http://wubaiqing.oss-cn-hangzhou.aliyuncs.com/1.png"></li>
-            <li data-id="1"><img src="http://www.meipin.com/static/images/2.png"></li>
-            <li data-id="2"><img src="http://www.meipin.com/static/images/3.png"></li>
-            <li data-id="3"><img src="http://www.meipin.com/static/images/4.png"></li>
+            <li data-id="1"><img src="http://wubaiqing.oss-cn-hangzhou.aliyuncs.com/2.png"></li>
+            <li data-id="2"><img src="http://wubaiqing.oss-cn-hangzhou.aliyuncs.com/3.png"></li>
+            <li data-id="3"><img src="http://wubaiqing.oss-cn-hangzhou.aliyuncs.com/4.png"></li>
         </ul>
         <em style="display: none;" data-id="0" class="fi_btn l"><a href="javascript:void(0);"></a></em><em data-id="1" class="fi_btn r"><a href="javascript:void(0);"></a></em><span class="close"><a href="javascript:void(0);" onclick="close_shortcut();"></a></span>
         <div class="fi_tab">
