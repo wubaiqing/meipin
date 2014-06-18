5a34f5f6eb339ac7b66a40d0e90b3e343c3ee372
909933794 <909933794@11.com>
2014-06-18 18:44:02 +0800

    qq互联

--- SQL/meipin_郭路路_20140618_qq互联.sql
+++ SQL/meipin_郭路路_20140618_qq互联.sql
@@ -1,18 +1,6 @@
-ALTER TABLE `meipin_users` MODIFY COLUMN `password`  varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '密码' AFTER `username`; //将密码设置为空
-
-ALTER TABLE `meipin_users` MODIFY COLUMN `email`  varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '邮箱' AFTER `password`; //邮箱设置为空
-
-ALTER TABLE `meipin_users`
-MODIFY COLUMN `salt`  varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '加密字符串' AFTER `email`;  //加密字符串 设置为空
-
-ALTER TABLE `meipin_users`
-MODIFY COLUMN `is_valid`  tinyint(1) NULL DEFAULT 0 COMMENT '邮箱是否验证' AFTER `last_dr_time`;
-//邮箱是否验证 设置为空
-
 ALTER TABLE `meipin_users`
 ADD COLUMN `qq_openid`  varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'qq登陆唯一openid' AFTER `mobile`;
   //添加qq登路唯一id
 
 ALTER TABLE `meipin_users_address`
 ADD COLUMN `email`  varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '添加邮箱' AFTER `updated_at`;
-
