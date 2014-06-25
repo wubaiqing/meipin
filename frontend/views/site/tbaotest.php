2121e40e04a1528d001158188f69d23c34bb145d
909933794 <909933794@11.com>
2014-06-25 13:25:29 +0800

    淘宝客链接测试

---
+++ frontend/views/site/tbaotest.php
@@ -0,0 +1,7 @@
+
+
+   <a data-type="0" biz-itemid="19886122002" data-tmpl="192x40" data-tmplid="625" data-rd="2" data-style="2" data-border="1" target="_blank">http://detail.tmall.com/item.htm?id=19886122002&mt=</a>
+
+
+   
+   <script type="text/javascript"> (function(win,doc){ var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0]; if (!win.alimamatk_show) { s.charset = "gbk"; s.async = true; s.src = "http://a.alimama.cn/tkapi.js"; h.insertBefore(s, h.firstChild); }; var o = { pid: "mm_56250611_6552067_23244345",/*推广单元ID，用于区分不同的推广渠道*/ appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/ unid: ""/*自定义统计字段*/ }; win.alimamatk_onload = win.alimamatk_onload || []; win.alimamatk_onload.push(o); })(window,document);</script> 
