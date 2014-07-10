5d59477793a74815f34c27001f8c30d6d199f051
liukui <liukui@55tuan.com>
2014-07-10 22:22:17 +0800

    update

--- common/extensions/alipayapi/alipay_submit.class.php
+++ common/extensions/alipayapi/alipay_submit.class.php
@@ -96,8 +96,8 @@ class AlipaySubmit {
 		
 		$sHtml = "<form id='alipaysubmit' name='alipaysubmit' action='".$this->alipay_gateway_new."_input_charset=".trim(strtolower($this->alipay_config['input_charset']))."' method='".$method."'>";
 		while (list ($key, $val) = each ($para)) {
-            $sHtml.= "<input type='hidden' name='".$key."' value='".$val."'/>";
-        }
+                    $sHtml.= "<input type='hidden' name='".$key."' value='".$val."'/>";
+                }
 
 		//submit按钮控件请不要含有name属性
 //                $sHtml = $sHtml."<input type='submit' value='".$button_name."'></form>";
