a258337a8961407bf39bbea7fb8cbf485a85a24d
wubaiqing <wubaiqing@vip.qq.com>
2014-07-11 10:21:44 +0800

    test

--- common/extensions/alipayapi/alipay_submit.class.php
+++ common/extensions/alipayapi/alipay_submit.class.php
@@ -102,10 +102,10 @@ class AlipaySubmit {
 		//submit按钮控件请不要含有name属性
 //                $sHtml = $sHtml."<input type='submit' value='".$button_name."'></form>";
                 $sHtml = $sHtml."$button_name</form>";
-		$sHtml = $sHtml."<script>$(document).ready(function (){
+		$sHtml = $sHtml."<script>
 			$('img').load(function () {
 				document.forms['alipaysubmit'].submit();
-			})})</script>";
+			})</script>";
 
 		return $sHtml;
 	}
