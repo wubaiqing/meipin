c64c66ffe095ece5fc56d718c61baef6c1df2756
liukui <liukui@55tuan.com>
2014-07-11 10:06:19 +0800

    update

--- frontend/views/layouts/exchange.php
+++ frontend/views/layouts/exchange.php
@@ -44,7 +44,7 @@
         <div style="display:none;">
             var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1000359564'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s22.cnzz.com/z_stat.php%3Fid%3D1000359564%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
         </div>
-        <div id="pay_confirm">
+        <div id="pay_confirm" style="display: none;">
             <div class="order_confirm">
                 <div><h2>请在新打开的页面完成支付</h2></div>
                 <div class="content">
