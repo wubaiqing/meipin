4f69d1ddf96f55fc678ca87d393d75d93637575d
wubaiqing <wubaiqing@vip.qq.com>
2014-07-11 10:09:57 +0800

    test

--- common/extensions/alipayapi/alipay_submit.class.php
+++ common/extensions/alipayapi/alipay_submit.class.php
@@ -103,7 +103,7 @@ class AlipaySubmit {
 //                $sHtml = $sHtml."<input type='submit' value='".$button_name."'></form>";
                 $sHtml = $sHtml."$button_name</form>";
 		
-		$sHtml = $sHtml."<script>$(function () {alert('123')})</script>";
+		$sHtml = $sHtml."<script>$(document).ready(function (){alert('123')})</script>";
 		
 		return $sHtml;
 	}
