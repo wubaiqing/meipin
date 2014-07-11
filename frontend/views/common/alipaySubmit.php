f414109e7cd32d905577910472a38685124b9ec8
liukui <liukui@55tuan.com>
2014-07-11 16:02:20 +0800

    update

--- frontend/views/common/alipaySubmit.php
+++ frontend/views/common/alipaySubmit.php
@@ -1,3 +1,10 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body>
 <div style="text-align: center;padding:20px;">
     <?php echo $title; ?>
 </div>
+        </body>
+</html>
