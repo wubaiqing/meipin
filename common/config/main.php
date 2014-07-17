6ae50b5f3339cb5fca1242037b3e3b3fe0c5ab47
wubaiqing <wubaiqing@vip.qq.com>
2014-07-17 14:26:11 +0800

    修改配置文件

--- common/config/main.php
+++ common/config/main.php
@@ -27,9 +27,9 @@ return [
             ],
         ],
         'db' => [
-            'connectionString' => 'mysql:host=localhost;dbname=meipin',
+            'connectionString' => 'mysql:host=114.215.202.199;dbname=meipin',
             'username' => 'root',
-            'password' => '0Dz9Bnlmpvs!',
+            'password' => '123456',
             'charset' => 'utf8',
             'tablePrefix' => '',
             'emulatePrepare' => true,
@@ -66,29 +66,5 @@ return [
         'exchangeLogPageSize' => 20,
         //分页最大缓存页面数
         'pageCahceMaxCount' => 5,
-        //物流系统
-        'logisticsSystem' =>[
-            1 => 'EMS',
-            2 => '申通速递',
-            3 => '圆通速递',
-            4 => '韵达快递',
-            5 => '中通快递',
-            6 => '顺丰速递',
-            7 => '天天快递',
-            8 => '百世汇通',
-            9 => '邮政包裹',
-            10 => '宅急送',
-        ],
-        //支付宝账号信息配置
-        'alipay' =>[
-            //收款账号
-            'email' => 'meipin2309@163.com',
-            //支付宝ID
-            'id' => '2088411283973406',
-            //支付宝验证码
-            'key' => '76suwy363u03pkgzbq1qq2b22z6ddnbb',
-        ],
-        //支付超时
-        'payTimeout' =>3600,
     ],
 ];
