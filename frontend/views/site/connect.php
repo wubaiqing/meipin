5d34a1d2ec40d75f397b913804ee7867edb86d60
909933794 <909933794@11.com>
2014-07-29 14:47:06 +0800

    修改联系我们

--- frontend/views/site/connect.php
+++ frontend/views/site/connect.php
@@ -1,4 +1,5 @@
 <div id="wrap_p" style='margin-top:38px;'>
    <h1 style="font-size:18px">积分换购问题  请联系QQ：932679251</h1><br/>
-   <h1 style="font-size:18px">其他问题请联系QQ：534095228</h1>
+   <h1 style="font-size:18px">其他问题请联系QQ：534095228</h1><br/>
+   <h1 style="font-size:18px">联系电话：010-85175413</h1>
 </div>
