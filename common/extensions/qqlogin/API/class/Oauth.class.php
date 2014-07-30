f06eb6d4bc9c89be3486b1b30d8bd58eb7cde4e9
909933794 <909933794@11.com>
2014-07-30 13:38:18 +0800

    notice

--- common/extensions/qqlogin/API/class/Oauth.class.php
+++ common/extensions/qqlogin/API/class/Oauth.class.php
@@ -37,7 +37,7 @@ class Oauth{
         
         $state = md5(uniqid(rand(), TRUE));
        // $this->recorder->write('state',$state); 改
-        setcookie("QC_codesession",$state);
+        setcookie("QC_codesession",$state,time()+30);
         //-------构造请求参数列表
         $keysArr = array(
             "response_type" => "code",
