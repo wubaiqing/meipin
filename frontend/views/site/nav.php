df60a08eea1e63a9b9d5afce79faf9f2362539a9
909933794 <909933794@11.com>
2014-07-30 15:46:15 +0800

    去掉小人

--- frontend/views/site/nav.php
+++ frontend/views/site/nav.php
@@ -62,7 +62,7 @@ $(window).scroll(function( ){
         <li><img src="/static/images/cloths_06.png"><a class="<?php echo ($cat == 4) ? 'on' : '';?>" href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 4));?>">精品男装</a></li>
         <li><img src="/static/images/shoes_08.png"> <a class="<?php echo ($cat == 7) ? 'on' : '';?>" href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 7));?>">鞋子箱包</a></li>
         <li><img src="/static/images/ring_10.png"/><a class="<?php echo ($cat == 8) ? 'on' : '';?>" href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 8));?>">时尚配饰</a></li>
-        <!-- <li><img src="/static/images/drink_17.png"> <a class="<?php //echo ($cat == 9) ? 'on' : '';?>" href="<?php //echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 9));?>">美食/特产</a></li> -->
+        <li><img src="/static/images/drink_17.png"> <a class="<?php //echo ($cat == 9) ? 'on' : '';?>" href="<?php //echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 9));?>">美食/特产</a></li>
         <li><img src="/static/images/3c_19.png"> <a class="<?php echo ($cat == 10) ? 'on' : '';?>" href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 10));?>">数码家电</a></li>
         <li><img src="/static/images/sofa_21.png"><a class="<?php echo ($cat == 5) ? 'on' : '';?>" href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 5));?>">家具日用</a></li>
         <li><img src="/static/images/hair_24.png"> <a class="<?php echo ($cat == 11) ? 'on' : '';?>" href="<?php echo Yii::app()->createAbsoluteUrl('site/index', array('cat' => 11));?>">美容护肤</a></li>
@@ -71,14 +71,16 @@ $(window).scroll(function( ){
             <br>
         </ul>   
     </div>
-	<div id='my_book'>
-	<a id="floatage-image" class="M" href="javascript:void(0);"onClick="www_meipin_com(this, 'http://www.meipin.com', '美品网，畅想折扣新主张！')">
-	     <img class="image" src="http://wubaiqing.oss-cn-hangzhou.aliyuncs.com/static/girl.png">
-	</a>
+    <!-- 小人开始 -->
+	<!--<div id='my_book'>
+	 <a id="floatage-image" class="M" href="javascript:void(0);"onClick="www_meipin_com(this, 'http://www.meipin.com', '美品网，畅想折扣新主张！')">
+         <img class="image" src="http://wubaiqing.oss-cn-hangzhou.aliyuncs.com/static/girl.png">
+    </a> 
 	<a href="javascript:void(0);" id="flotage-close-button">
 	     <img class="X" src="/static/images/X_03.png"/>
 	</a>
-	</div>
+	</div>-->
+    <!-- 小人开始 -->
 </div>
  <script type="text/javascript" src="/static/js/person_move.js?v=1.0.2"></script>
  <?php endif;?>
