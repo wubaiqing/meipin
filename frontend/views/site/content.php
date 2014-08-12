566689f19a5039228b9f6087fb7de8ad9d86c334
909933794 <909933794@11.com>
2014-08-12 13:14:29 +0800

    样式修改

--- frontend/views/site/content.php
+++ frontend/views/site/content.php
@@ -68,8 +68,8 @@
                     <h4>
                         <span>
                             <em>
-                                <b style="font-size:16px;font-weight:normal;"><?php echo $item->active_price;?><b style="font-size:12px;font-weight:bold;">元</b>+</b>
-                                <em style="font-size:16px;font-weight:bold;"><?php echo $item->integral; ?>分</em>
+                                <b style="font-size:28px;font-weight:'微软雅黑';"><?php echo $item->active_price;?><b style="font-size:12px;font-weight:bold;">元</b>+<b style="font-size:28px;font-weight:'微软雅黑';"><?php echo $item->integral; ?>分</b></b>
+                                
                             </em>
                         </span>
                         <span>
