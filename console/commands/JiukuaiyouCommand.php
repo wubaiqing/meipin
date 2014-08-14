6cc512c3308e6fe0279fed629f03d9c0b2395b45
909933794 <909933794@11.com>
2014-08-14 14:03:53 +0800

    no object

--- console/commands/JiukuaiyouCommand.php
+++ console/commands/JiukuaiyouCommand.php
@@ -101,10 +101,12 @@ class JiukuaiyouCommand extends CConsoleCommand
         $data['title'] = $dealad->find('h3', 0)->find('a', 0)->plaintext;
 
         // 商品价格
-        //$data['price'] = FetchHelpers::getInt($dealad->find('h4', 0)->find('span', 0)->plaintext);
+        $price = $dealad->find('h4', 0)->find('span', 0)->plaintext;
+        $data['price'] = FetchHelpers::getInt($price);
 
         // 商品原始价格
-        //$data['origin_price'] = FetchHelpers::getInt($dealad->find('h4', 0)->find('span', 2)->plaintext);
+        $origin_price = $dealad->find('h4', 0)->find('span', 2)->plaintext;
+        $data['origin_price'] = FetchHelpers::getInt($origin_price);
 
 	    // 关联网站
 	    $data['relation_website'] = 3;
