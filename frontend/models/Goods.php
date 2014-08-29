699a6ef945674ca5b84f2dfee818cf9cd20beccb
909933794 <909933794@11.com>
2014-08-29 13:59:57 +0800

    开始时间

--- frontend/models/Goods.php
+++ frontend/models/Goods.php
@@ -260,7 +260,7 @@ class Goods extends ActiveRecord implements IArrayable
         $criteria->select = '*, FROM_UNIXTIME(t.start_time, "%Y-%m-%d") as day';
 
        if ($hot == 0) {
-            $criteria->order = 't.head_show DESC, day DESC, t.list_order DESC';
+            $criteria->order = 't.head_show DESC, t.start_time DESC, t.list_order DESC';
         } else {
             $criteria->order = 't.start_time DESC';
         }
