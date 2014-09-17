c0113a81ff125b0c6de9d8e42f5321f8ffd1a525
909933794 <909933794@11.com>
2014-09-17 14:09:55 +0800

    淘宝优站接口

--- frontend/controllers/ApiController.php
+++ frontend/controllers/ApiController.php
@@ -112,7 +112,7 @@ class ApiController extends Controller
             if($val->content)
             //$content = iconv('UTF-8', 'GBK//IGNORE', $val->content);
             $content = mb_convert_encoding( $val->content, 'GBK', 'UTF-8'); 
-            $str .= "INSERT INTO `meipin_shai` (id,username,content,ptime,img,goods_id,updated_at,created_at,is_delete) VALUES ('{$val->id}', '{$username}', '{$content}', '{$val->ptime}', '{$val->img}', '{$val->goods_id}', '{$val->updated_at}', '{$val->created_at}', '{$val->is_delete}');<br/>";
+            $str .= "INSERT INTO `shai` (id,username,content,ptime,img,goods_id,updated_at,created_at,is_delete) VALUES ('{$val->id}', '{$username}', '{$content}', '{$val->ptime}', '{$val->img}', '{$val->goods_id}', '{$val->updated_at}', '{$val->created_at}', '{$val->is_delete}');<br/>";
         }
         echo $str;
     }
