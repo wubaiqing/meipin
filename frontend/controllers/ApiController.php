34ad30c923e361d8a135ffe03ae7dcd8f08e8026
909933794 <909933794@11.com>
2014-09-18 13:28:03 +0800

    修改淘宝优站接口

--- frontend/controllers/ApiController.php
+++ frontend/controllers/ApiController.php
@@ -72,7 +72,8 @@ class ApiController extends Controller
                 $findtime = date("Y-m-d H:i:s",$val->updated_at);
                 $pbuy = $val->pbuy?$val->pbuy:0;
                 $pnum = $val->pnum?$val->pnum:0;
-                $str .= "insert into huodong (id,cid,gourl,title,imgurl,yuanjia,huodongjia,starttime,endtime,findtime,dianpuleixing,shangpinfenlei,paixu,is_zhe800,change_price,mark,pnum,pbuy,comment)values('{$val->id}','{$val->tb_id}','{$item_url[0]}','{$title}','{$pic_url[0]}','{$val->origin_price}','{$val->price}','{$starttime}','{$endtime}','{$findtime}','b','{$catname}','{$val->list_order}','{$val->is_zhe800}','{$val->change_price}','{$val->mark}','{$pnum}','{$pbuy}','{$comment}');";
+                $mark = $val->mark?$val->mark:0;
+                $str .= "insert into huodong (id,cid,gourl,title,imgurl,yuanjia,huodongjia,starttime,endtime,findtime,dianpuleixing,shangpinfenlei,paixu,is_zhe800,change_price,mark,pnum,pbuy,comment)values('{$val->id}','{$val->tb_id}','{$item_url[0]}','{$title}','{$pic_url[0]}','{$val->origin_price}','{$val->price}','{$starttime}','{$endtime}','{$findtime}','b','{$catname}','{$val->list_order}','{$val->is_zhe800}','{$val->change_price}','{$mark}','{$pnum}','{$pbuy}','{$comment}');";
             }
 		 }
          //$file_pointer = fopen("aa.sql","a+");        
