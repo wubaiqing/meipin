70c1eb8b8d6977f71d33f27c584542d8a9904536
909933794 <909933794@11.com>
2014-09-19 10:39:57 +0800

    晒点评论图片修改

--- backend/views/shai/_admin.php
+++ backend/views/shai/_admin.php
@@ -1,14 +1,13 @@
 <tr>
     <td class="center"><?php echo $data->id ?></td>
-    <td><?php //echo $data->goods_id;
+    <?php //echo $data->goods_id;
       $goodsUrl =  Des::encrypt($data->goods_id);?>
-      <a href="http://www.meipin.com/out/<?php echo $goodsUrl;?>.html" target="_blank">链接</a></td>
-    <td><?php 
+    <?php 
     	$img = $data->img;
     	$imgarr = explode(';', $img);
-    	//echo $imgarr[0];
     ?>
-    <img src="<?php echo $imgarr[0];?>" /></td>
+    <td>
+    <a href="http://www.meipin.com/out/<?php echo $goodsUrl;?>.html" target="_blank"><img src="<?php echo $imgarr[0];?>" /></a></td>
     <td><?php echo $data->ptime; ?></td>
 
 
