0273663011bf92767634b4933777025106c8b68f
909933794 <909933794@11.com>
2014-09-23 15:18:00 +0800

    修改接口

--- frontend/controllers/ApiController.php
+++ frontend/controllers/ApiController.php
@@ -73,7 +73,7 @@ class ApiController extends Controller
                 $pbuy = $val->pbuy?$val->pbuy:0;
                 $pnum = $val->pnum?$val->pnum:0;
                 $mark = $val->mark?$val->mark:0;
-                $str .= "insert into huodong (id,cid,gourl,title,imgurl,yuanjia,huodongjia,starttime,endtime,findtime,dianpuleixing,shangpinfenlei,paixu,is_zhe800,change_price,mark,pnum,pbuy,comment)values('{$val->id}','{$val->tb_id}','{$item_url[0]}','{$title}','{$pic_url[0]}','{$val->origin_price}','{$val->price}','{$starttime}','{$endtime}','{$findtime}','b','{$catname}','{$val->list_order}','{$val->is_zhe800}','{$val->change_price}','{$mark}','{$pnum}','{$pbuy}','{$comment}');<br/>";
+                $str .= "insert into huodong (id,cid,gourl,title,imgurl,yuanjia,huodongjia,starttime,endtime,findtime,dianpuleixing,shangpinfenlei,paixu,is_zhe800,change_price,mark,pnum,pbuy,comment)values('{$val->id}','{$val->tb_id}','{$item_url[0]}','{$title}','{$pic_url[0]}','{$val->origin_price}','{$val->price}','{$starttime}','{$endtime}','{$findtime}','b','{$catname}','{$val->list_order}','{$val->is_zhe800}','{$val->change_price}','{$mark}','{$pnum}','{$pbuy}','{$comment}')@<br/>";
             }else{
                 //echo $title."<br/>";
             }
@@ -115,7 +115,7 @@ class ApiController extends Controller
             if($val->content)
             //$content = iconv('UTF-8', 'GBK//IGNORE', $val->content);
             $content = mb_convert_encoding( $val->content, 'GBK', 'UTF-8'); 
-            $str .= "INSERT INTO `shai` (id,username,content,ptime,img,goods_id,updated_at,created_at,is_delete) VALUES ('{$val->id}', '{$username}', '{$content}', '{$val->ptime}', '{$val->img}', '{$val->goods_id}', '{$val->updated_at}', '{$val->created_at}', '{$val->is_delete}');<br/>";
+            $str .= "INSERT INTO `shai` (id,username,content,ptime,img,goods_id,updated_at,created_at,is_delete) VALUES ('{$val->id}', '{$username}', '{$content}', '{$val->ptime}', '{$val->img}', '{$val->goods_id}', '{$val->updated_at}', '{$val->created_at}', '{$val->is_delete}')@<br/>";
         }
         echo $str;
     }
