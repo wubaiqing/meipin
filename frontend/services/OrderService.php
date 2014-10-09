ff9d31d9bb707c9d8d569b1ee07d1fc4b0314c3a
wubaiqing <wubaiqing@vip.qq.com>
2014-10-09 21:56:57 +0800

    解决超时问题

--- frontend/services/OrderService.php
+++ frontend/services/OrderService.php
@@ -47,6 +47,7 @@ class OrderService
         if (($order->created_at + $maxTimeout) < time()) {
 	        $order->pay_status = 1;
 	        $order->update(['pay_status']);
+	        ExchangeLog::deleteWelfareCache($order->user_id, 1, 1);
             return CommonHelper::getDataResult(false, ['message' => '付款时间已经超时，不能再进行付款', 'url' => Yii::app()->createUrl("order/list")]);
         }
         $html = self::alipayapi($order->order_id, $goodsInfo['name'], $order->pay_price, $goodsInfo['url'], $goodsInfo['remark']);
